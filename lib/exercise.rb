fe6c95ccd66b2ea23f58de75935917bdad79a0a6
Lindsay-R <lrecknell@gmail.com>
2014-07-28 11:15:28 -0600

    story 6

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -8,4 +8,12 @@ require "./lib/order"
 
 #p Customer.first.name
 
-p Customer.last 
+#p Customer.last
+
+
+#### the two methods below return the same thing
+# p Customer.find(3)
+#
+# p Customer.where(id:3).first
+
+
